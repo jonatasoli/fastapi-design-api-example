618447586fe2ade74a1a7a799d81a1490cca5ddb
Jonatas Oliveira <contato@jonatasoliveira.me>
2021-06-24 16:46:20 -0300

    add update payment status

--- app/order/service/business_rules.py
+++ app/order/service/business_rules.py
@@ -1,4 +1,4 @@
-from fastapi import status
+from fastapi import status, HTTPException
 
 from order.adapters.orm_adapter import ordermodel
 
@@ -8,20 +8,54 @@ class Order:
         return await ordermodel.create(obj_in=data)
 
     async def update_status_waiting_payment(self, id):
-        ...
+        _order = await ordermodel.get(id)
+        if not hasattr(_order, "payment_status") or _order.payment_status != "Waiting Payment":
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Update payment status is not permitted"
+            )
+        _order.payment_status = "paid"
+        return await ordermodel.update(obj_id=id, obj_in=_order)
 
     async def cancel(self, id):
-        ...
+        _order = await ordermodel.get(id)
+        if not hasattr(_order, "payment_status") or _order.payment_status != "Waiting Payment":
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Update payment status is not permitted"
+            )
+        _order.payment_status = "Cancelled"
+        return await ordermodel.update(obj_id=id, obj_in=_order)
 
     async def status(self, id):
-        ...
+        _order = await ordermodel.get(id)
+        if not hasattr(_order, "payment_status") or _order.payment_status != "Waiting Payment":
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Update payment status is not permitted"
+            )
+        return _order
 
 
 class Payment:
     async def process(self, id):
-        ...
+        _order = await ordermodel.get(id)
+        if not hasattr(_order, "payment_status") or _order.payment_status != "Waiting Payment":
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Update payment status is not permitted"
+            )
+        _order.payment_status = "Paid"
+        return await ordermodel.update(obj_id=id, obj_in=_order)
 
 
 class Receipt:
     async def delivery(self, id):
-        ...
+        _order = await ordermodel.get(id)
+        if not hasattr(_order, "payment_status") or _order.payment_status != "Waiting Payment":
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Update payment status is not permitted"
+            )
+        _order.payment_status = "Delivery"
+        return await ordermodel.update(obj_id=id, obj_in=_order)
--- app/order/tests/test_unit_order_service.py
+++ app/order/tests/test_unit_order_service.py
@@ -1,12 +1,27 @@
 import pytest
+from fastapi import HTTPException, status
 
 from order.service.business_rules import Order
 from order.domains.order import orderCreateResponse, orderUpdateResponse, orderGetResponse, orderBase
 from order.service.business_rules import Order, Payment, Receipt
 
+
 base_data = orderBase(
         product_name="Latte",
         payment_status="Created",
+        total_amount=1000,
+)
+get_response_update = orderGetResponse(
+        id=1,
+        product_name="Latte",
+        payment_status="Waiting Payment",
+        total_amount=1000,
+)
+
+get_response_conflict = orderGetResponse(
+        id=1,
+        product_name="Latte",
+        payment_status="Paid",
         total_amount=1000,
 )
 
@@ -38,3 +53,34 @@ async def test_create_rule(mocker):
     order = Order()
     output = await order.create(data=base_data)
     assert output == response_create
+
+
+@pytest.mark.asyncio
+async def test_update_payment_status_conflict(mocker):
+    mocker.patch(
+        "order.adapters.orm_adapter.ordermodel.update",
+        return_value=response_update
+    )
+    mocker.patch(
+        "order.adapters.orm_adapter.ordermodel.get",
+        return_value=get_response_conflict
+    )
+    order = Order()
+    with pytest.raises(HTTPException) as exc_info:
+        await order.update_status_waiting_payment(id=1)
+    assert exc_info.value.status_code == status.HTTP_409_CONFLICT
+
+
+@pytest.mark.asyncio
+async def test_update_payment_status(mocker):
+    mocker.patch(
+        "order.adapters.orm_adapter.ordermodel.update",
+        return_value=response_update
+    )
+    mocker.patch(
+        "order.adapters.orm_adapter.ordermodel.get",
+        return_value=get_response_update
+    )
+    order = Order()
+    output = await order.update_status_waiting_payment(id=1)
+    assert output == response_update
